ac89e019fdfb32dcefd66808b35ae9ea1cb1f3bb
lyceeayme <94358485+lyceeayme@users.noreply.github.com>
2022-11-14 12:51:58 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,21 +4,21 @@ require '..\vendor\autoload.php';
 
 use App\Controller\TestAutoload;
 use App\Exceptions\AppException;
+
 /**
  * Description of index
  *
  * @author ayme.pignon
  */
-
 //Declaration de constants d'application
 define('DS', DIRECTORY_SEPARATOR);
-define('RACINE', new DirectoryIterator(dirname(__FILE__)). DS . ".." .DS);
-include_once(RACINE.DS.'config/conf.php');
+define('RACINE', new DirectoryIterator(dirname(__FILE__)) . DS . ".." . DS);
+include_once(RACINE . DS . 'config/conf.php');
 include_once(PATH_VENDOR . "autoload.php");
-include_once(RACINE.DS.'includes/params.php');
+include_once(RACINE . DS . 'includes/params.php');
 
-try{
-   if ((!array_key_exists('c', $_GET)) || (!array_key_exists('a', $_GET))) {
+try {
+    if ((!array_key_exists('c', $_GET)) || (!array_key_exists('a', $_GET))) {
         throw new Exception("Erreur, cette page n'existe pas");
     }
     $BaseController = filter_input(INPUT_GET, 'c', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -38,15 +38,15 @@ try{
         throw new Error("Le contrôleur demandé n'existe pas");
     }
 } catch (Error $ex) {
-    $error['message'] = $ex->getMessage();    
-    $error['mode'] = MODE_DEV;  
-    MyTwig::afficheVue('errors/error.html.twig', $error );
+    $error['message'] = $ex->getMessage();
+    $error['mode'] = MODE_DEV;
+    MyTwig::afficheVue('errors/error.html.twig', $error);
 } catch (AppException $ex) {
-     $error['message'] = $ex->getMessage();
-     $error['mode'] = MODE_DEV;  
-    MyTwig::afficheVue('errors/error.html.twig', $error);    
+    $error['message'] = $ex->getMessage();
+    $error['mode'] = MODE_DEV;
+    MyTwig::afficheVue('errors/error.html.twig', $error);
 } catch (Exception $ex) {
-     $error['message'] = $ex->getMessage();
-     $error['mode'] = MODE_DEV;  
+    $error['message'] = $ex->getMessage();
+    $error['mode'] = MODE_DEV;
     MyTwig::afficheVue('errors/error.html.twig', $error);
 } 
