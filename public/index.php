1bb898e88525077052a1a9fb1f5fb31bead17e57
lyceeayme <94358485+lyceeayme@users.noreply.github.com>
2022-11-16 13:16:48 +0100

    Changement de l'index

--- public/index.php
+++ public/index.php
@@ -1,16 +1,7 @@
 <?php
 
-require '..\vendor\autoload.php';
-
-use App\Controller\TestAutoload;
 use App\Exceptions\AppException;
 
-/**
- * Description of index
- *
- * @author ayme.pignon
- */
-//Declaration de constants d'application
 define('DS', DIRECTORY_SEPARATOR);
 define('RACINE', new DirectoryIterator(dirname(__FILE__)) . DS . ".." . DS);
 include_once(RACINE . DS . 'config/conf.php');
@@ -23,13 +14,7 @@ try {
     }
     $BaseController = filter_input(INPUT_GET, 'c', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $action = filter_input(INPUT_GET, 'a', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    // TODO : contrôleur à écrire plus tard
-    if (empty($BaseController)) {
-        $BaseController = 'Identification';
-        $action = 'login';
-    }
     $controller = "App\\Controller\\" . $BaseController . "Controller";
-
     if (class_exists($controller, true)) {
         $c = new $controller();
         $params = array(array_slice($_REQUEST, 2));
@@ -38,15 +23,12 @@ try {
         throw new Error("Le contrôleur demandé n'existe pas");
     }
 } catch (Error $ex) {
-    $error['message'] = $ex->getMessage();
-    $error['mode'] = MODE_DEV;
-    MyTwig::afficheVue('errors/error.html.twig', $error);
+    include(PATH_VIEW . '/errors/error.html');
 } catch (AppException $ex) {
-    $error['message'] = $ex->getMessage();
-    $error['mode'] = MODE_DEV;
-    MyTwig::afficheVue('errors/error.html.twig', $error);
+    include(PATH_VIEW . '/errors/error.html');
 } catch (Exception $ex) {
-    $error['message'] = $ex->getMessage();
-    $error['mode'] = MODE_DEV;
-    MyTwig::afficheVue('errors/error.html.twig', $error);
+    include(PATH_VIEW . '/errors/error.html');
 } 
+
+    
+
--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -1,15 +1,90 @@
 <?php
+declare(strict_types=1);
+namespace App\Entity;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
- */
-
-/**
- * Description of Client
- *
- * @author ayme.pignon
- */
 class Client {
-    //put your code here
+
+    private  int $id;
+    private  string $titreCli;
+    private  string $nomCli;
+    private  string $prenomCli;
+    private  string $adresseRue1Cli;
+    private  ?string $adresseRue2Cli;
+    private  string $cpCli;
+    private  string $villeCli;
+    private  string $telCli;
+
+    public function __construct(array $params = null) {
+        if (!is_null($params)) {
+            foreach ($params as $cle => $valeur) {
+                if(strlen($valeur)>0){
+                $this->$cle = $valeur;
+                } else{
+                     $this->$cle = null;
+                }
+            }
+        }
+    }
+
+    public function getId() :int {
+        return $this->id;
+    }
+
+    public function getTitreCli() :string  {
+        return $this->titreCli;
+    }
+
+    public function getNomCli() :string {
+        return filter_var($this->nomCli, FILTER_SANITIZE_FULL_SPECIAL_CHARS) ;
+        //return $this->nomCli;
+    }
+
+    public function getPrenomCli() :string  {
+        return $this->prenomCli;
+    }
+
+    public function getAdresseRue1Cli() :string {
+        return $this->adresseRue1Cli;
+    }
+
+    public function getAdresseRue2Cli(): ?string  {
+        return $this->adresseRue2Cli;
+    }
+
+    public function getCpCli():string  {
+        return $this->cpCli;
+    }
+
+    public function getVilleCli() :string {
+        return $this->villeCli;
+    }
+
+    public function getTelCli() :string {
+        return $this->telCli;
+    }
+
+    public function __toString() :string {
+        return "Le client dont le numéro est égal à " . $this->noCli . " s'appelle " . $this->titreCli . " " . $this->nomCli . " " . $this->prenomCli . "<br>";
+    }
+    
+    public function setAdresseRue1Cli(string $adresseRue1Cli): void {
+        $this->adresseRue1Cli = $adresseRue1Cli;
+    }
+
+    public function setAdresseRue2Cli(string $adresseRue2Cli): void {
+        $this->adresseRue2Cli = $adresseRue2Cli;
+    }
+
+    public function setCpCli(string $cpCli): void {
+        $this->cpCli = $cpCli;
+    }
+
+    public function setVilleCli(string $villeCli): void {
+        $this->villeCli = $villeCli;
+    }
+
+    public function setTelCli(string $telCli): void {
+        $this->telCli = $telCli;
+    }
+
 }
