877031f51997d79c76f3f428a57173839f85d214
lyceeayme <94358485+lyceeayme@users.noreply.github.com>
2022-11-16 13:12:48 +0100

    ajout class client

---
+++ src/Entity/Client.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+/**
+ * Description of Client
+ *
+ * @author ayme.pignon
+ */
+class Client {
+    //put your code here
+}
